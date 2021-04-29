8db68594502dc9e2ae89e2492520827e8155213a
Marius Pran <mariusbpran@gmail.com>
2021-04-29 14:36:20 +0200

    added basic bat movement

--- src/Enemies/Enemy.gd
+++ src/Enemies/Enemy.gd
@@ -3,14 +3,38 @@ extends KinematicBody2D
 class_name Enemy
 
 onready var stats = $Stats
+onready var playerDetectionZone =  $PlayerDetectionZone
 const BatDeathEffect = preload("res://src/Enemies/Bat/bat_death_effect.tscn")
  
-var velocity = Vector2.ZERO
+enum {
+	IDLE,
+	WANDER,
+	CHASE
+}
 
+var state = CHASE
+
+var velocity = Vector2.ZERO
 
 func _physics_process(delta):
 	velocity = velocity.move_toward(Vector2.ZERO, stats.resistance*delta)
+	
+	match state:
+		IDLE:
+			velocity = velocity.move_toward(Vector2.ZERO, stats.friction*delta)
+			seek_player()
+		WANDER:
+			pass
+		CHASE:
+			var player = playerDetectionZone.player
+			if player != null:
+				var direction = (player.global_position - global_position).normalized()
+				velocity = velocity.move_toward(direction * stats.speed, stats.acceleration)
 	velocity = move_and_slide(velocity)
+
+func seek_player():
+	if playerDetectionZone.can_see_player():
+		state = CHASE
 
 func on_death():
 	var batDeathEffect = BatDeathEffect.instance()
