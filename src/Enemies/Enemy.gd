a961ecc7a26c33befefb8ede9d881b2c813dc1d6
Marius Pran <mariusbpran@gmail.com>
2021-04-29 14:01:39 +0200

    testing

--- src/Enemies/Enemy.gd
+++ src/Enemies/Enemy.gd
@@ -7,6 +7,7 @@ const BatDeathEffect = preload("res://src/Enemies/Bat/bat_death_effect.tscn")
  
 var velocity = Vector2.ZERO
 
+
 func _physics_process(delta):
 	velocity = velocity.move_toward(Vector2.ZERO, stats.resistance*delta)
 	velocity = move_and_slide(velocity)
