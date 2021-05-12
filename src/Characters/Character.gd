6f51d5c7f07617c7880ad6ebaac5b753cf8a69dc
Marius Pran <mariusbpran@gmail.com>
2021-05-12 21:12:35 +0200

    Minor bugfixes

--- src/Characters/Character.gd
+++ src/Characters/Character.gd
@@ -7,14 +7,7 @@ onready var animationState = animationTree.get("parameters/playback")
 onready var swordHitbox = $HitboxPivot/SwordHitbox
 onready var hurtbox = $Hurtbox
 
-var stats = PlayerStats
-var HitPoints = 100
-var knockback = 1
-var resistance = 0
-var weight = 1
-var speed = 100
-var acceleration = 20
-var friction = 20
+const stats = PlayerStats
 var velocity = Vector2.ZERO
 
 enum {
@@ -28,7 +21,7 @@ var state = MOVE
 func _ready():
 	stats.connect("no_health", self, "queue_free")
 	animationTree.active = true
-	swordHitbox.knockback_vector = Vector2.RIGHT
+	swordHitbox.knockback_vector = Vector2.DOWN
 
 func _physics_process(delta):
 	match state:
@@ -39,15 +32,15 @@ func _physics_process(delta):
 		DODGE:
 			dodge_state()
 
-func move_state(delta):
+func move_state(_delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
-		velocity += input_vector * stats.acceleration * delta
-		velocity = velocity.clamped(stats.speed * delta)
+		velocity += input_vector * stats.acceleration
+		velocity = velocity.clamped(stats.speed)
 		swordHitbox.knockback_vector = input_vector
 		
 		animationState.travel("Run")
@@ -55,20 +48,23 @@ func move_state(delta):
 		animationTree.set("parameters/Run/blend_position", input_vector)
 		animationTree.set("parameters/Attack/blend_position", input_vector)
 	else:
-		velocity = velocity.move_toward(Vector2.ZERO, stats.friction*delta)
+		velocity = velocity.move_toward(Vector2.ZERO, stats.friction)
 		animationState.travel("Idle")
 	
-	move_and_collide(velocity)
+	velocity = move_and_slide(velocity)
 	if Input.is_action_just_pressed("attack"):
 		state = ATTACK
 
-func attack_state(delta):
+func attack_state(_delta):
 	animationState.travel("Attack")
-	move_and_collide(velocity)
-	velocity = velocity.move_toward(Vector2.ZERO, friction*delta/5)
+	velocity = move_and_slide(velocity)
+	velocity = velocity.move_toward(Vector2.ZERO, stats.friction)
 	
+var count = 0
 func attack_animation_finished():
 	state = MOVE
+	print(count)
+	count += 1
 	
 func dodge_state():
 	pass
