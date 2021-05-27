5d8ba51feab22127436a5e678a5768ff06d21b7d
Marius B. Pran <Mariusbpran@gmail.com>
2021-05-27 14:23:03 +0200

    Bat AI update
    Bats now move away from each other if they are too close, instead of colliding.
    bats no longer have a collision box.
    bats now wander, and are "anchored" to their starting position

--- src/UI/GUI.gd
+++ src/UI/GUI.gd
@@ -4,10 +4,10 @@ onready var HP_value = $HBoxContainer/Bars/Health/Count/Background/Number
 onready var HP_bar = $HBoxContainer/Bars/Health/Gauge
 
 func update_health_UI(value):
-	HP_value.text = str(value)
+	HP_value.text = str(round(value))
 	HP_bar.value = value
 	
 func _ready():
 	var _error = PlayerStats.connect("health_changed", self, "update_health_UI")
-	HP_value.text = str(PlayerStats.health)
+	HP_value.text = str(round(PlayerStats.health))
 	HP_bar.value = PlayerStats.health
